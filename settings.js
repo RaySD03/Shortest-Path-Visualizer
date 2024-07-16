1794b93dc22c06fe37e88042e578de8f56752392
Raymond Babayan <113494325+RaySD03@users.noreply.github.com>
2024-07-16 11:33:10 -0700

    Updated settings.js
    Relocated grid settings from grid.js to settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,10 @@
+const column_slider = document.getElementById('ColumnRangeSlider');
+const column_count = document.getElementById('ColumnRange');
+const row_slider = document.getElementById('RowRangeSlider');
+const row_count = document.getElementById('RowRange');
+const variance_range = document.getElementById('VarianceRange');
+const variance_slider = document.getElementById('VarianceRangeSlider');
+
 function openSwipeMenu() {
     const menu = document.querySelector('.swipe-menu');
     menu.style.right = '0'; // Slide the menu in from the right
@@ -7,3 +14,21 @@ function closeSwipeMenu() {
     const menu = document.querySelector('.swipe-menu');
     menu.style.right = '-300px'; // Slide the menu out to the right
 }
+
+// Update the current column_slider value
+column_slider.oninput = function() {
+    column_count.innerHTML = this.value;
+    createGrid(row_count.innerHTML, column_count.innerHTML, variance_range.innerHTML);
+};
+
+// Update the current row_slider value
+row_slider.oninput = function() {
+    row_count.innerHTML = this.value;
+    createGrid(row_count.innerHTML, column_count.innerHTML, variance_range.innerHTML);
+};
+
+// Update the current variance_slider value
+variance_slider.oninput = function() {
+    variance_range.innerHTML = this.value;
+    createGrid(row_count.innerHTML, column_count.innerHTML, variance_range.innerHTML);
+};
